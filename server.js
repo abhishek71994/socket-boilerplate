d641e08a2b05d4347279b5fc8af79d0240f7c989
abhishek71994 <abhishek71994@gmail.com>
2018-03-04 21:01:40 +0530

    boilerplate server update

--- server.js
+++ server.js
@@ -10,27 +10,6 @@ app.get("/",function(req,res){
 	res.sendFile(__dirname + "/index.html");
 });
 
-io.sockets.on('connection',function(socket){
-	connections.push(socket);
-	console.log(`Users connected: ${connections.length}`);
-	socket.on('disconnect',function(socket){
-		connections.splice(connections.indexOf(socket),1);
-		console.log(`Users connected: ${connections.length}`);
-	});
-	socket.on('send message',function(data){
-		io.sockets.emit('new message',{ msg: data });
-	});
-	socket.on('new user',function(data,callback){
-		callback = true;
-		socket.username = data;
-		users.push(socket.username);
-		updateUserNames();
-		io.sockets.emit('new message',{ msg: data });
-	})
-	function updateUserNames(){
-		
-	}
-});
 //listening to the server
 server.listen(process.env.PORT || 3000,function(){
 	console.log("The server is running");
